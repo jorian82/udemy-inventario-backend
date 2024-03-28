c6283798ba210c3915631abd05ef06c607278aad
Jorge Rivera <jorge.rivera@ssde.com.mx>
2024-03-28 00:06:05 -0600

    Udemy-01. Primera adición del proyecto base REST API control de inventario

---
+++ src/test/java/com/ssde/rest/inventarioapi/InventarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssde.rest.inventarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
